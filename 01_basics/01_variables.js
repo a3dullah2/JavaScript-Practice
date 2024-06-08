806b3b28a0ceb19574a2f6710dbc7e6a9c6aeab2
a3dullah2 <157971135+a3dullah2@users.noreply.github.com>
2024-06-08 06:43:01 +0000

    variable practice

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 14453
+let accountEmail = "opman@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lahore"
+let accountState;
+
+
+// accountId = 2 // not allowed
+console.log(accountId);
+
+accountEmail = "newemail@gmail.com"
+accountPassword = "212121"
+accountCity = "Karachi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
