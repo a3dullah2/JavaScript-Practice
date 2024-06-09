92164bccd511c06ffec7b1959f260cefc039cbb5
a3dullah2 <157971135+a3dullah2@users.noreply.github.com>
2024-06-09 07:22:57 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,14 @@
+let str1 = "hello"
+let str2 = " abdullah"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2")
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
