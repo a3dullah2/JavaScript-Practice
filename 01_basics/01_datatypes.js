af96df88335a7b042d74ad91625aa6824c6ef91d
a3dullah2 <157971135+a3dullah2@users.noreply.github.com>
2024-06-09 06:24:44 +0000

    data types

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all JS code as newer verison
+
+// alert(3 + 3)  using node js not browser
+ 
+
+let name  = "Abdullah"// string
+let age = 19 //integer
+let isLoggedIn = false //boolean
+let state = null //stand alone value
+
+console.log(typeof "abdullah" );
+
+
+
